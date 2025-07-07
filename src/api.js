6d8e0664f5c173fd552a7da406ef8dac0ee61eb3
somu <jyatiskadey@gmail.com>
2025-07-07 15:40:59 +0530

    first push

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // ✅ adjust if deployed
+  baseURL: "https://store-backend-xr00.onrender.com/api", // ✅ adjust if deployed
 });
 
 // Optional: You can add interceptors here if using auth
